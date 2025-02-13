a47651f371f82fd3ef5f2ee1fdec3c003413312f
Mawuko <mawukoefoe@gmail.com>
2025-02-13 17:44:36 +0000

    Ajout des challenges 46

---
+++ challenge_46.py
@@ -0,0 +1,10 @@
+# Ask the user to enter a number. Keep asking until they enter 
+# a value over 5 and then display the message "The last number was a 
+# [number]" and stop program.
+
+number = 0
+
+while number < 5:
+    number = int(input("Enter a number : "))
+
+print("The last  number was",number)
