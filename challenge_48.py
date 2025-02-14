ac7733b3cdd315f1d461e7d9fff438713f593c43
Mawuko <mawukoefoe@gmail.com>
2025-02-14 12:14:38 +0000

    Ajout des info des challenges 48 et 50

---
+++ challenge_48.py
@@ -0,0 +1,6 @@
+# Ask for the name of somebody the user wants to invite to a party. After this
+# dipslay the message "[name] has now be invited" and add 1 to the count. Then
+# ask if they want to invite sombody else. Keep repeating this until the no longer
+# want to invite anyone else to the party and then display how many poeple they have
+# comming to the party
+
