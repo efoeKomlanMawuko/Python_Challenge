28e257f4eae3438c3282bd57a3ec8a6ea2247397
Mawuko <mawukoefoe@gmail.com>
2025-02-14 14:38:12 +0000

    Ajout des info du challenge 49

---
+++ challenge_49.py
@@ -0,0 +1,5 @@
+# Create a variable called compnum and set the value to 50. Ask the user to
+# enter a number. While their guess is not the same as the compnum value, tell
+# them if their guess is too low or too high and ask them to have another guess.
+# if they enter a same value as compnum, display the message "Well done, you took
+# [count] attempts".
