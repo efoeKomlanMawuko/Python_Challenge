ef38b33c749df67f4306ddf44ae03d9756386740
efoeKomlanMawuko <mawukoefoe@gmail.com>
2025-02-24 10:07:23 +0000

    Ajout des Challenges 52 et 53

---
+++ challenge_52.py
@@ -0,0 +1,6 @@
+# display a random integer between 1 and 100 incluse
+
+import random
+
+num = random.randint(1,100)
+print(num)
---
+++ challenge_53.py
@@ -0,0 +1,5 @@
+# Display a random fruit from a list of five fruits.
+import random
+
+num = random.choice(["Mangue","Orange","Papaye","Bannane","Anana"])
+print(num)
