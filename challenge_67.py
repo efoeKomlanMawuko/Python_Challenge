55640a3235f9f412c73444c54419b9a2e66d0166
Mawuko <mawukoefoe@gmail.com>
2025-02-25 17:37:02 +0000

    Ajout du Challenge 67

---
+++ challenge_67.py
@@ -0,0 +1,11 @@
+# Create a pattern
+import turtle
+import random
+
+for i in range(0,10):
+    for i in range(0,8):
+        turtle.pensize(2)
+        turtle.forward(50)
+        turtle.right(45)
+    turtle.right(36)
+turtle.exitonclick()
